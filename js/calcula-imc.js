f262454c730f8a281beb26efee88080c91c67220
joaogostoso15 <116820618+joaogostoso15@users.noreply.github.com>
2022-11-10 07:43:45 -0300

    Update calcula-imc.js

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -1,5 +1,5 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista";
+titulo.textContent = "JOÃO GOSTOSO";
 
 var pacientes = document.querySelectorAll(".paciente");
 
